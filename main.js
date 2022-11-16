d003e53f2d857c3cd538bc774178f585f7ce3543
EC2 Default User <ec2-user@ip-172-31-21-141.ap-northeast-1.compute.internal>
2022-11-16 16:25:01 +0000

    stopwatch課題再提出

--- main.js
+++ main.js
@@ -1,55 +1,43 @@
-var startButton;
-var stopButton;
-var resetButton;
-var showTime;
+const time = document.getElementById('time');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
 
-var timer;
-var startTime;
-var elapsedTime = 0;
-var holdTime = 0;
+let startTime;
+let stopTime = 0;
+let timeoutID;
 
-window.onload = function () {
-    startButton = document.getElementById("start");
-    stopButton = document.getElementById("stop");
-    resetButton = document.getElementById("reset");
-    showTime = document.getElementById("time");
+function displayTime() {
+  const currentTime = new Date(Date.now() - startTime + stopTime);
+  const h = String(currentTime.getHours()-9).padStart(1, '0');
+  const m = String(currentTime.getMinutes()).padStart(1, '0');
+  const s = String(currentTime.getSeconds()).padStart(1, '0');
+  const ms = String(currentTime.getMilliseconds()).padStart(1, '0');
+
+  time.textContent = `${h}:${m}:${s}.${ms}`;
+  timeoutID = setTimeout(displayTime, 50);
 }
 
-function start(){
-    startTime = Date.now();
-    measureTime();
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  resetButton.disabled = false;
+  startTime = Date.now();
+  displayTime();
+});
 
-    startButton.disabled = true;
-    stopButton.disabled = false;
-    resetButton.disabled = false;
-}
+stopButton.addEventListener('click', function() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = false;
+  clearTimeout(timeoutID);
+  stopTime += (Date.now() - startTime);
+});
 
-function stop(){
-    clearInterval(timer);
-    holdTime += Date.now() - startTime;
-
-    startButton.disabled = false;
-    stopButton.disabled = true;
-    resetButton.disabled = false;
-}
-
-
-function reset(){
-    clearInterval(timer);
-
-    elapsedTime = 0;
-    holdTime = 0;
-    showTime.textContent = "0:0:0:0";
-
-    startButton.disabled = false;
-    stopButton = true;
-    resetButton.disabled = true;
-}
-
-function measureTime() {
-    timer = setTimeout(function () {
-        elapsedTime = Date.now() - startTime + holdTime;
-        showTime.textContent = new Date(elapsedTime).toISOString().slice(12, 23)
-        measureTime();
-    }, 20);
-}
+resetButton.addEventListener('click', function() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = true;
+  time.textContent = '0:0:0:0';
+  stopTime = 0;
+});
